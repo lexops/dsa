766add6ad04919d2bb818886c249f0e90953e752
lexops <134613820+lexops@users.noreply.github.com>
2024-12-24 09:34:32 -0300

    style: lint code

--- linked_list.c
+++ linked_list.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+struct node
+{
     int value;
     struct node *next;
 };
 typedef struct node node_t;
 
-node_t *add_node(node_t *head, int value) {
+node_t *add_node(node_t *head, int value)
+{
     node_t *new_node = malloc(sizeof(node_t));
-    if (new_node == NULL) {
+    if (new_node == NULL)
+    {
         return NULL;
     }
 
-    if (head == NULL) {
+    if (head == NULL)
+    {
         new_node->value = value;
         new_node->next = NULL;
         return new_node;
-    } else {
+    }
+    else
+    {
         node_t *tmp = head;
         new_node->value = value;
         new_node->next = tmp;
@@ -27,19 +33,23 @@ node_t *add_node(node_t *head, int value) {
     }
 }
 
-void print_list(node_t *head) {
+void print_list(node_t *head)
+{
     node_t *curr = head;
-    while (curr != NULL) {
+    while (curr != NULL)
+    {
         printf("%d -> ", curr->value);
         curr = curr->next;
     }
     printf("NULL\n");
 }
 
-node_t *revert_list(node_t *head) {
+node_t *revert_list(node_t *head)
+{
     node_t *curr = head;
     node_t *prev = NULL;
-    while (curr->next != NULL) {
+    while (curr->next != NULL)
+    {
         node_t *tmp = curr->next;
         curr->next = prev;
         prev = curr;
@@ -48,10 +58,12 @@ node_t *revert_list(node_t *head) {
     return prev;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     node_t *head = NULL;
 
-    for (int i = 0; i < argc; i++) {
+    for (int i = 0; i < argc; i++)
+    {
         head = add_node(head, atoi(argv[i]));
     }
 
